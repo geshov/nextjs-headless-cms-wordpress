168713d54041caa77a5d4babbbf5cd6a7526e560
gesho <geshov@gmail.com>
2025-11-13 22:36:20 +0300

    fix message

--- src/ui/posts/notfound/notfound.tsx
+++ src/ui/posts/notfound/notfound.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import notfound from "./notfound.svg";
 
+const message = "Ничего не найдено";
+
 export function Notfound() {
   return (
     <div className="flex flex-col justify-center items-center pt-6">
@@ -9,11 +11,11 @@ export function Notfound() {
         width={notfound.width}
         height={notfound.height}
         className="w-auto max-w-80"
-        alt="Ничего не найдено"
+        alt={message}
       />
 
       <div className="text-3xl font-bold text-[#eb725f] text-center -translate-y-6">
-        Ничего не найдено
+        {message}
       </div>
     </div>
   );
