ebfad6f48af38a140129049ce48469e21211b25f
gesho <geshov@gmail.com>
2025-11-15 11:15:55 +0300

    fix loading css

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
-import styles from "./loading.module.css";
+import css from "./loading.module.css";
 
 export default function Loading() {
   return (
     <div
-      className={`bg-secondary fixed top-0 right-0 left-0 z-50 h-1 ${styles.loading}`}
+      className={`bg-secondary fixed top-0 right-0 left-0 z-50 h-1 ${css.loading}`}
       style={{ transform: "translateX(-100%)" }}></div>
   );
 }
