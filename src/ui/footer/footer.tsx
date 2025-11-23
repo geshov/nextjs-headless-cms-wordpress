97eb87b501cd26eba01c92f7e1c3ee060e83316e
gesho <geshov@gmail.com>
2025-11-23 12:06:50 +0300

    fix style

--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content/50 p-4">
+    <footer className="footer footer-center bg-base-300 text-secondary/75 p-4">
       <aside>
         <a
           href="https://github.com/geshov/nextjs-headless-cms-wordpress"
