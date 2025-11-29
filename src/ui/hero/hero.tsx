a17127c1e3b2fa0835a8fb05adfa7c4c2e01d66a
gesho <geshov@gmail.com>
2025-11-29 23:54:10 +0300

    fix style

--- src/ui/hero/hero.tsx
+++ src/ui/hero/hero.tsx
@@ -1,9 +1,30 @@
+import { Blob } from "./blob/blob";
 import { Button } from "./button/button";
 
 export function Hero() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center">
+    <div className="relative hero bg-base-200 min-h-screen overflow-hidden">
+      <div className="absolute top-0 left-1/3 w-80">
+        <div className="relative -left-1/2 text-cyan-950 light:text-cyan-200">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute right-1/3 w-80">
+        <div className="relative -right-1/2 text-violet-950 light:text-violet-300">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-1/2 w-80">
+        <div className="relative -left-3/4 text-blue-950 light:text-blue-300">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-3xl"></div>
+
+      <div className="relative hero-content text-center">
         <div className="max-w-lg">
           <h1 className="text-5xl text-balance font-bold">
             Статьи из Headless CMS
