4e03c284f861a548f0c3ef91701e865edf4aacc5
gesho <geshov@gmail.com>
2025-11-29 11:56:01 +0300

    fix style on scroll

--- src/ui/navbar/navbar.tsx
+++ src/ui/navbar/navbar.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Logo } from "./logo/logo";
 import { Theme } from "./theme/theme";
 
 export function Navbar() {
+  const [scroll, setScroll] = useState(() =>
+    typeof window === "undefined" ? 0 : window.scrollY
+  );
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  useEffect(() => {
+    addEventListener("scroll", handleScroll);
+    return () => {
+      removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed top-0 left-0 right-0 backdrop-blur-md z-10">
+    <div
+      className={`fixed top-0 left-0 right-0  z-10 ${
+        scroll && "backdrop-blur-md"
+      }`}>
       <div className="flex justify-between items-center w-full max-w-7xl mx-auto px-4 py-3">
         <Logo />
         <Theme />
