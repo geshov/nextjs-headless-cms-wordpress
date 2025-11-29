7d987b63007b5af0e10877ca08b5f3d7697ce746
gesho <geshov@gmail.com>
2025-11-29 12:25:11 +0300

    fix style

--- src/ui/navbar/navbar.tsx
+++ src/ui/navbar/navbar.tsx
@@ -15,15 +15,13 @@ export function Navbar() {
 
   useEffect(() => {
     addEventListener("scroll", handleScroll);
-    return () => {
-      removeEventListener("scroll", handleScroll);
-    };
+    return () => removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div
-      className={`fixed top-0 left-0 right-0  z-10 ${
-        scroll && "backdrop-blur-md"
+      className={`fixed top-0 left-0 right-0 z-10 ${
+        scroll && "backdrop-blur-md shadow-md"
       }`}>
       <div className="flex justify-between items-center w-full max-w-7xl mx-auto px-4 py-3">
         <Logo />
