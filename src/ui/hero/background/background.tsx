19d75afd0f32a8decca5155c5c805d77cc4e44c5
gesho <geshov@gmail.com>
2025-11-30 17:24:16 +0300

    fix style

--- src/ui/hero/background/background.tsx
+++ src/ui/hero/background/background.tsx
@@ -3,20 +3,20 @@ import { Blob } from "./blob/blob";
 export function Background() {
   return (
     <>
-      <div className="absolute top-0 left-1/3 w-80">
+      <div className="absolute top-0 left-1/3 w-md">
         <div className="relative -left-1/2 text-cyan-950 light:text-cyan-200">
           <Blob />
         </div>
       </div>
 
-      <div className="absolute right-1/3 w-80">
+      <div className="absolute right-1/3 w-md">
         <div className="relative -right-1/2 text-violet-950 light:text-violet-300">
           <Blob />
         </div>
       </div>
 
-      <div className="absolute bottom-0 left-1/2 w-80">
-        <div className="relative -left-3/4 text-blue-950 light:text-blue-300">
+      <div className="absolute bottom-0 left-1/2 w-md">
+        <div className="relative -left-2/3 text-blue-950 light:text-blue-300">
           <Blob />
         </div>
       </div>
