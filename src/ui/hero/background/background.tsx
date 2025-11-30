f73bb27a3ba9e52b26d3d09872a82434adcfa9a6
gesho <geshov@gmail.com>
2025-11-30 10:49:52 +0300

    fix components

---
+++ src/ui/hero/background/background.tsx
@@ -0,0 +1,27 @@
+import { Blob } from "./blob/blob";
+
+export function Background() {
+  return (
+    <>
+      <div className="absolute top-0 left-1/3 w-80">
+        <div className="relative -left-1/2 text-cyan-950 light:text-cyan-200">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute right-1/3 w-80">
+        <div className="relative -right-1/2 text-violet-950 light:text-violet-300">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-1/2 w-80">
+        <div className="relative -left-3/4 text-blue-950 light:text-blue-300">
+          <Blob />
+        </div>
+      </div>
+
+      <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-3xl"></div>
+    </>
+  );
+}
--- src/ui/hero/hero.tsx
+++ src/ui/hero/hero.tsx
@@ -1,28 +1,10 @@
-import { Blob } from "./blob/blob";
+import { Background } from "./background/background";
 import { Button } from "./button/button";
 
 export function Hero() {
   return (
     <div className="relative hero bg-base-200 min-h-screen overflow-hidden">
-      <div className="absolute top-0 left-1/3 w-80">
-        <div className="relative -left-1/2 text-cyan-950 light:text-cyan-200">
-          <Blob />
-        </div>
-      </div>
-
-      <div className="absolute right-1/3 w-80">
-        <div className="relative -right-1/2 text-violet-950 light:text-violet-300">
-          <Blob />
-        </div>
-      </div>
-
-      <div className="absolute bottom-0 left-1/2 w-80">
-        <div className="relative -left-3/4 text-blue-950 light:text-blue-300">
-          <Blob />
-        </div>
-      </div>
-
-      <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-3xl"></div>
+      <Background />
 
       <div className="relative hero-content text-center">
         <div className="max-w-lg">
