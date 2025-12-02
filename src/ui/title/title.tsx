c28ea730cdace3679f6b243eb587b83a5a964362
gesho <geshov@gmail.com>
2025-12-02 23:40:39 +0300

    fix style

--- src/ui/title/title.tsx
+++ src/ui/title/title.tsx
@@ -3,17 +3,17 @@
 export function Title({ post }: { post: any }) {
   return (
     <div className="pt-20 pb-6 bg-base-200">
-      <div className="max-w-7xl mx-auto p-4">
+      <div className="max-w-7xl mx-auto p-4 space-y-8">
         <div className="prose">
           <h1>{post.title.rendered}</h1>
+        </div>
 
-          <div className="font-semibold">
-            {new Date(post.date).toLocaleDateString("ru-RU", {
-              day: "numeric",
-              month: "long",
-              year: "numeric",
-            })}
-          </div>
+        <div className="font-semibold">
+          {new Date(post.date).toLocaleDateString("ru-RU", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
         </div>
       </div>
     </div>
