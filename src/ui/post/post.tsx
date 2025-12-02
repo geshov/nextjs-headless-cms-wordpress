331568cb7d0f20c7681fc3e09e08568701b48c18
gesho <geshov@gmail.com>
2025-12-02 17:43:27 +0300

    fix style

--- src/ui/post/post.tsx
+++ src/ui/post/post.tsx
@@ -7,7 +7,7 @@ import { Other } from "./other/other";
 
 export function Post({ post }: { post: any }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-5 gap-6 py-10">
+    <div className="grid grid-cols-1 md:grid-cols-5 gap-x-6 gap-y-10 py-10">
       <div className="md:col-span-3 space-y-8">
         <div className="max-w-lg">
           <Picture post={post} />
@@ -16,7 +16,7 @@ export function Post({ post }: { post: any }) {
         <Content post={post} />
       </div>
 
-      <div className="md:col-span-2 space-y-8 pt-6 md:pt-0 md:ps-6 border-t md:border-t-0 md:border-l border-base-content/15">
+      <div className="md:col-span-2 space-y-8 pt-8 md:pt-0 md:ps-6 border-t md:border-t-0 md:border-l border-base-content/15">
         <div className="prose">
           <h2>Другие статьи</h2>
         </div>
