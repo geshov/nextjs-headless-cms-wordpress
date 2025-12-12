91cfb9d383cc254f376f3e7e2b9ca83e8d761e33
gesho <geshov@gmail.com>
2025-12-12 12:24:13 +0300

    fix background

--- src/ui/background/background.tsx
+++ src/ui/background/background.tsx
@@ -3,19 +3,19 @@ import { Blob } from "./blob/blob";
 export function Background() {
   return (
     <>
-      <div className="fixed -top-1/6 right-1/4 w-1/3">
+      <div className="fixed bottom-6/12 right-1/4 w-2/3 sm:w-1/2 md:w-1/3">
         <div className="relative -right-1/2 text-cyan-950 light:text-cyan-100">
           <Blob />
         </div>
       </div>
 
-      <div className="fixed left-1/4 w-1/3">
+      <div className="fixed bottom-4/12 left-1/4 w-2/3 sm:w-1/2 md:w-1/3">
         <div className="relative -left-1/2 text-violet-950 light:text-violet-200">
           <Blob />
         </div>
       </div>
 
-      <div className="fixed -bottom-1/6 right-1/2 w-1/3">
+      <div className="fixed top-6/12 right-1/2 w-2/3 sm:w-1/2 md:w-1/3">
         <div className="relative -right-2/3 text-blue-950 light:text-blue-200">
           <Blob />
         </div>
